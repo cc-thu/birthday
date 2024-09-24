00a6c278ee961f4b6ca8aa6ac9ed46158b4295e4
cc-thu <3221456875@qq.com>
2024-09-25 01:32:49 +0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -25,6 +25,5 @@ $(function () {
 });
 
 $(function () {
-    $('#love_text').append(config.loveText);
-    $('#love_text').css('display', 'block');
+    $('#love_text').append(config.loveText)
 });
